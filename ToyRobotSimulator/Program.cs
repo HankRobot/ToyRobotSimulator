f726f69c92299fa54916d98f5316358f3c1a1dbb
Hank <tzehankchia@gmail.com>
2024-07-10 20:25:02 +0100

    Add project files.

---
+++ ToyRobotSimulator/Program.cs
@@ -0,0 +1,83 @@
+﻿using ToyRobotSimulator;
+
+string command = "";
+int x, y;
+Direction direction;
+string[] data;
+Console.WriteLine("This is a Toy Robot Simulator. The robot moves in a 5x5 grid depending on the direction it is facing.");
+Console.WriteLine("Please use the commands below:");
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("PLACE X,Y,F");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Put the toy robot on the table in position X,Y and facing NORTH, SOUTH, EAST or WEST");
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("LEFT");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Turns the robot counterclockwise.");
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("RIGHT");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Turns the robot clockwise.");
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("MOVE");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Moves the robot forward depending on the direction its facing.");
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("REPORT");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Displays the current position and direction of the robot.");
+Robot robot = new Robot();
+
+while (true)
+{
+    command = Console.ReadLine();
+
+    if (command.Contains("PLACE", StringComparison.OrdinalIgnoreCase))
+    {
+        data = command.Replace("PLACE ", "").Split(',');
+        if (data.Length == 3)
+        {
+            if (!Int32.TryParse(data[0], out x))
+            {
+                Console.WriteLine("X coordinate input error; Try a number from 0 to 4");
+            }
+            if (!Int32.TryParse(data[1], out y))
+            {
+                Console.WriteLine("Y coordinate input error; Try a number from 0 to 4");
+            }
+            if (!Enum.TryParse(data[2], out direction))
+            {
+                Console.WriteLine("Direction input error; Try NORTH, EAST, SOUTH OR WEST");
+            }
+            robot.SetPosition(x, y, direction);
+        }
+        else
+        {
+            Console.WriteLine("Wrong format!");
+        }
+    }
+    else if (robot.isPlaced())
+    {
+        if (command == "MOVE")
+        {
+            robot.MovePosition();
+        }
+        else if (command == "LEFT")
+        {
+            robot.TurnLeft();
+        }
+        else if (command == "RIGHT")
+        {
+            robot.TurnRight();
+        }
+        else if (command == "REPORT")
+        {
+            robot.ReportPosition();
+        }
+    }
+    else
+    {
+        Console.WriteLine("Robot is not placed.");
+    }
+}
+
---
+++ ToyRobotSimulator/Robot.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyRobotSimulator
+{
+    /// <summary>
+    /// Direction of the robot to face
+    /// </summary>
+    enum Direction
+    {
+        NORTH,
+        EAST,
+        SOUTH,
+        WEST
+    }
+
+    internal class Robot
+    {
+        private int _x, _y;
+        private bool _isplaced;
+        private Direction _direction;
+
+        /// <summary>
+        /// Creates an object of the class Robot
+        /// </summary>
+        public Robot() 
+        {
+            _isplaced = false;  
+        }
+
+        /// <summary>
+        /// Checks if the robot is placed or not.
+        /// </summary>
+        /// <returns>returns true if the robot is placed, else returns false</returns>
+        public bool isPlaced() 
+        {
+            return _isplaced; 
+        }  
+
+        /// <summary>
+        /// Sets the robot at a position with x and y coordinates with direction NORTH, EAST, SOUTH or WEST
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="direction"></param>
+        public void SetPosition(int x, int y, Direction direction)
+        {
+            _isplaced = true;
+            if (x >= 0 && x <= 4 && y >= 0 && y <= 4)
+            {
+                _x = x;
+                _y = y;
+                _direction = direction;
+            }
+            else
+            {
+                Console.WriteLine("Out of bounds!");
+            }
+        }
+
+        /// <summary>
+        /// Displays the positions and direction of the robot
+        /// </summary>
+        public void ReportPosition()
+        {
+            Console.WriteLine($"Current Position: {_x},{_y},{_direction}");
+        }
+
+        /// <summary>
+        /// Turns the robot clockwise
+        /// </summary>
+        public void TurnRight()
+        {
+            if (_direction == Direction.WEST)
+            {
+                _direction = Direction.NORTH;
+            }
+            else 
+            {
+                _direction++;
+            }
+        }
+
+        /// <summary>
+        /// Turns the robot counterclockwise
+        /// </summary>
+        public void TurnLeft()
+        {
+            if (_direction == Direction.NORTH)
+            {
+                _direction= Direction.WEST;    
+            }
+            else
+            {
+                _direction--;
+            }
+        }
+
+        /// <summary>
+        /// Moves the robot forward depending on the direction it is facing
+        /// </summary>
+        public void MovePosition()
+        {
+            switch (_direction)
+            {
+                case Direction.NORTH:
+                    if (_y == 4)
+                    {
+                        Console.WriteLine("Invalid direction! Out of bounds!");
+                        ReportPosition();
+                        return;
+                    }
+                    _y++;
+                    break;
+                case Direction.SOUTH:
+                    if (_y == 0)
+                    {
+                        Console.WriteLine("Invalid direction! Out of bounds!");
+                        ReportPosition();
+                        return;
+                    }
+                    _y--;
+                    break;
+                case Direction.EAST:
+                    if (_x == 4) {
+                        Console.WriteLine("Invalid direction! Out of bounds!");
+                        ReportPosition();
+                        return;
+                    }
+                    _x++;
+                    break;
+                case Direction.WEST:
+                    if (_x == 0)
+                    {
+                        Console.WriteLine("Invalid direction! Out of bounds!");
+                        ReportPosition();
+                        return;
+                    }    
+                    _x--;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
